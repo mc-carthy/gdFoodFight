77982f63a65778ecae01ee65ba095778669c276a
Mick McCarthy <mick@mccarthy.tech>
2020-03-03 21:37:46 +0000

    Add animation tree player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,10 +3,12 @@ extends KinematicBody
 const SPEED = 10
 var motion = Vector3()
 
-func _process(delta):
-	move(delta)
 
-func move(delta):
+func _process(delta):
+	move()
+
+
+func move():
 	var dx = 0
 	var dz = 0
 	
@@ -18,6 +20,8 @@ func move(delta):
 		dx += 1
 	if Input.is_action_pressed('left'):
 		dx -= 1
+	
+
 	
 	motion = Vector3(dx, 0, dz) * SPEED
 	move_and_slide(motion, Vector3.UP)
